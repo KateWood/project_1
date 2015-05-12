7bf754b1047f93f78671494ec6315fda28fa7ea0
Kate Wood <katewood611@gmail.com>
2015-05-12 14:58:35 -0700

    adds working tie function

--- Vanilla/js/testing.js
+++ Vanilla/js/testing.js
@@ -67,12 +67,20 @@ function getWinner() {
         }
         return;
     }
+    // check for tie
+    var cellEmpty = false;
+    for (var i = 0; i < boxes.length; i++) {
+      console.log(boxes[i].innerHTML);
+      if (boxes[i].innerHTML == "") {
+        cellEmpty = true;
+      }
+    }
+    if (cellEmpty === false) {
+      alert("It's a tie!");
+      for (var i = 0; i < boxes.length; i++) {
+          boxes[i].innerHTML = null;
+      }
+    }
+
 }
-
-
-
-
-
-
-
 
