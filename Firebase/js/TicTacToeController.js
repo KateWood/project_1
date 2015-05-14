2e8611e95c2e8ae6bfdfa139885e6b5b20b9a242
Kate Wood <katewood611@gmail.com>
2015-05-14 16:36:42 -0700

    saves changes to local boxes to firebase, so moves made show in two browser windows

--- Firebase/js/TicTacToeController.js
+++ Firebase/js/TicTacToeController.js
@@ -5,7 +5,9 @@ angular
         // injects $firebaseArray
         TicTacToeController.$inject = ['$firebaseArray'];
 
-        // TicTacToeController function -- must contain everything for the controller
+        /*
+         TicTacToeController function -- must contain everything for the controller
+        */
         function TicTacToeController ($firebaseArray) {
             
             // variable capture
@@ -22,7 +24,7 @@ angular
             // sets game status as game in progress
             self.gameStatus = "Game in progress";
 
-            // gets boxes array from firebase
+            // gets boxes array from firebase and attaches it to the controller
             self.boxes = (function(){
                 var ref = new Firebase('https://tictactoekate.firebaseio.com/boxes');
                 var boxes = $firebaseArray(ref);
@@ -42,7 +44,9 @@ angular
             // attaches resetBoard to the controller
             self.resetBoard = resetBoard;
 
-            // alternates between players 1 and 2
+            /*
+             alternates between players 1 and 2
+            */
             function takeTurns() {
                 
                 self.turn++;
@@ -55,7 +59,11 @@ angular
 
             }
 
-            // reacts to box being clicked if game is in progress
+            /* 
+             reacts to box being clicked if game is in progress
+
+             @param number index - provides the index of the box that was clicked
+            */
             function chooseBox(index) {
                 
                 if (self.gameStatus === "Game in progress") {
@@ -71,15 +79,31 @@ angular
                     }
                     else if (myTurn === "x") {
                         self.boxes[index].isX = true;
+
                     }
+                    self.boxes.$save(self.boxes[index]);
                     self.getWinner();
                 }
 
             }
 
-            // checks for a winner
+            /* 
+             checks for a winner or tie
+            */
             function getWinner() {
                 
+                // var token = ["x", "o"];
+                // for (var i = 0; i < tokens.length; i++)
+                //     var t = tokens[i];
+                //     var winner_found = false
+                //     if 
+
+                //     if(t == "x") {
+                //         // player 1 wins, increase p1 score, return
+                //     } else {
+                //         //player 2 wins, increase p2 score, return
+                //     }
+
                 // checks for x win
                 if (
                     ((self.boxes[0].isX === true) 
@@ -107,6 +131,10 @@ angular
                         && (self.boxes[4].isX === true) 
                         && (self.boxes[6].isX === true))
                 ){
+                    /*
+                     changes gameStatus & p1 score if x has won
+                     stops looking for a win or tie
+                    */
                     self.gameStatus = "X wins!";
                     self.p1++;
                     return;
@@ -139,6 +167,10 @@ angular
                         && (self.boxes[4].isO === true) 
                         && (self.boxes[6].isO === true))
                 ){
+                    /*
+                     changes gameStatus & p2 score if o has won
+                     stops looking for a win or tie
+                    */
                     self.gameStatus = "O wins!";
                     self.p2++;
                     return;
@@ -152,24 +184,31 @@ angular
                     }
                 }
                 if (cellEmpty === false) {
+                    /*
+                     changes gameStatus & tie count if it is a tie
+                    */
                     self.gameStatus = "It's a tie!"
                     self.tie++;
                 }
             
             }
 
-            // clears board when game is over
+            /*
+             clears board when game is over
+            */
             function resetBoard() {
                 
-                // prevents a game reset when the game is in progress
+                // prevents a game reset when a game is in progress
                 if (self.gameStatus === "Game in progress") {
                     alert("Whoa! One game at a time!");
                     return;
                 }
+                // clears the board for a new game
                 else {
                     for (var i = 0; i < self.boxes.length; i++) {
                         self.boxes[i].isX = false;
                         self.boxes[i].isO = false;
+                        self.boxes.$save(self.boxes[i]);
                     }
 
                     //resets the game status for the new game
