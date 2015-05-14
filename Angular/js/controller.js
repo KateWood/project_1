632639ddc25921cf45eec8e7b48cdabb4f2dc12c
Kate Wood <katewood611@gmail.com>
2015-05-13 17:00:26 -0700

    angular functionality is complete - last commit before beginning to add firebase

--- Angular/js/controller.js
+++ Angular/js/controller.js
@@ -36,69 +36,66 @@ function TicTacToeController () {
     //attaches getWinner to the controller
     self.getWinner = getWinner;
 
+    //attaches resetBoard to the controller
+    self.resetBoard = resetBoard;
+
     // alternates between players 1 and 2
     function takeTurns() {
         self.turn++;
         if (self.turn % 2 === 0) {
-          return "o";
+            return "o";
         } else {
-          return "x";
+            return "x";
         } 
     }
 
-    // reacts to box being clicked
+    // reacts to box being clicked if game is in progress
     function chooseBox(index) {
-        if ((self.boxes[index].isX === true) || (self.boxes[index].isO === true)) {
-          alert("Oops! This cell is already taken. Please select a different cell.");
-          return;
+        if (self.gameStatus === "Game in progress") {
+            if ((self.boxes[index].isX === true) || (self.boxes[index].isO === true)) {
+            alert("Oops! This cell is already taken. Please select a different cell.");
+            return;
+            }
+            var myTurn = takeTurns();
+            if (myTurn === "o") {
+                self.boxes[index].isO = true;
+            }
+            else if (myTurn === "x") {
+                self.boxes[index].isX = true;
+            }
+            self.getWinner();
         }
-        var myTurn = takeTurns();
-        if (myTurn === "o") {
-          self.boxes[index].isO = true;
-        }
-        else if (myTurn === "x") {
-          self.boxes[index].isX = true;
-        }
-        self.getWinner();
     }
 
     //checks for a winner
     function getWinner() {
-        //check for x win
+        //checks for x win
         if (((self.boxes[0].isX === true) && (self.boxes[1].isX === true) && (self.boxes[2].isX === true)) ||
-           ((self.boxes[3].isX === true) && (self.boxes[4].isX === true) && (self.boxes[5].isX === true)) ||
-           ((self.boxes[6].isX === true) && (self.boxes[7].isX === true) && (self.boxes[8].isX === true)) ||
-           ((self.boxes[0].isX === true) && (self.boxes[3].isX === true) && (self.boxes[6].isX === true)) ||
-           ((self.boxes[1].isX === true) && (self.boxes[4].isX === true) && (self.boxes[7].isX === true)) ||
-           ((self.boxes[2].isX === true) && (self.boxes[5].isX === true) && (self.boxes[8].isX === true)) ||
-           ((self.boxes[0].isX === true) && (self.boxes[4].isX === true) && (self.boxes[8].isX === true)) ||
-           ((self.boxes[2].isX === true) && (self.boxes[4].isX === true) && (self.boxes[6].isX === true)))
+            ((self.boxes[3].isX === true) && (self.boxes[4].isX === true) && (self.boxes[5].isX === true)) ||
+            ((self.boxes[6].isX === true) && (self.boxes[7].isX === true) && (self.boxes[8].isX === true)) ||
+            ((self.boxes[0].isX === true) && (self.boxes[3].isX === true) && (self.boxes[6].isX === true)) ||
+            ((self.boxes[1].isX === true) && (self.boxes[4].isX === true) && (self.boxes[7].isX === true)) ||
+            ((self.boxes[2].isX === true) && (self.boxes[5].isX === true) && (self.boxes[8].isX === true)) ||
+            ((self.boxes[0].isX === true) && (self.boxes[4].isX === true) && (self.boxes[8].isX === true)) ||
+            ((self.boxes[2].isX === true) && (self.boxes[4].isX === true) && (self.boxes[6].isX === true)))
         {
-          alert('x wins!');
-          self.p1++;
-          for (var i = 0; i < self.boxes.length; i++) {
-          self.boxes[i].isX = false;
-          self.boxes[i].isO = false;
-          }
-          return;
+            self.gameStatus = "X wins!";
+            self.p1++;
+            return;
         }
-        //check for o win
+        //checks for o win
         if (((self.boxes[0].isO === true) && (self.boxes[1].isO === true) && (self.boxes[2].isO === true)) ||
-           ((self.boxes[3].isO === true) && (self.boxes[4].isO === true) && (self.boxes[5].isO === true)) ||
-           ((self.boxes[6].isO === true) && (self.boxes[7].isO === true) && (self.boxes[8].isO === true)) ||
-           ((self.boxes[0].isO === true) && (self.boxes[3].isO === true) && (self.boxes[6].isO === true)) ||
-           ((self.boxes[1].isO === true) && (self.boxes[4].isO === true) && (self.boxes[7].isO === true)) ||
-           ((self.boxes[2].isO === true) && (self.boxes[5].isO === true) && (self.boxes[8].isO === true)) ||
-           ((self.boxes[0].isO === true) && (self.boxes[4].isO === true) && (self.boxes[8].isO === true)) ||
-           ((self.boxes[2].isO === true) && (self.boxes[4].isO === true) && (self.boxes[6].isO === true)))
+            ((self.boxes[3].isO === true) && (self.boxes[4].isO === true) && (self.boxes[5].isO === true)) ||
+            ((self.boxes[6].isO === true) && (self.boxes[7].isO === true) && (self.boxes[8].isO === true)) ||
+            ((self.boxes[0].isO === true) && (self.boxes[3].isO === true) && (self.boxes[6].isO === true)) ||
+            ((self.boxes[1].isO === true) && (self.boxes[4].isO === true) && (self.boxes[7].isO === true)) ||
+            ((self.boxes[2].isO === true) && (self.boxes[5].isO === true) && (self.boxes[8].isO === true)) ||
+            ((self.boxes[0].isO === true) && (self.boxes[4].isO === true) && (self.boxes[8].isO === true)) ||
+            ((self.boxes[2].isO === true) && (self.boxes[4].isO === true) && (self.boxes[6].isO === true)))
         {
-          alert('o wins!');
-          self.p2++;
-          for (var i = 0; i < self.boxes.length; i++) {
-          self.boxes[i].isX = false;
-          self.boxes[i].isO = false;
-          }
-          return;
+            self.gameStatus = "O wins!";
+            self.p2++;
+            return;
         }
         //checks for tie
         var cellEmpty = false;
@@ -108,13 +105,18 @@ function TicTacToeController () {
             }
         }
         if (cellEmpty === false) {
-            alert("It's a tie!");
-            for (var i = 0; i < self.boxes.length; i++) {
-          self.boxes[i].isX = false;
-          self.boxes[i].isO = false;
-          }
+            self.gameStatus = "It's a tie!"
         }
     
+    }
+
+    //clears board when game is over
+    function resetBoard() {
+        for (var i = 0; i < self.boxes.length; i++) {
+            self.boxes[i].isX = false;
+            self.boxes[i].isO = false;
+        }
+        self.gameStatus = "Game in progress";
     }
 
 }
