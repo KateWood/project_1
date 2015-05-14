46171a6873b0e3a1d79e38cec690cc796d775341
Kate Wood <katewood611@gmail.com>
2015-05-13 19:23:41 -0700

    adds connection to Firebase array

--- Firebase/js/controller.js
+++ Firebase/js/controller.js
@@ -2,124 +2,125 @@ angular
     .module('myApp')
         .controller("TicTacToeController", TicTacToeController)
 
-function TicTacToeController () {
-    var self = this;
+        //injects $firebaseArray
+        TicTacToeController.$inject = ['$firebaseArray'];
 
-    // starts the turn counter at zero
-    self.turn = 0;
+        function TicTacToeController ($firebaseArray) {
+            var self = this;
 
-    // starts both player scores at zero
-    self.p1 = 0;
-    self.p2 = 0;
+            // starts the turn counter at zero
+            self.turn = 0;
 
-    //sets game status as game in progress
-    self.gameStatus = "Game in progress";
+            // starts both player scores and tie count at zero
+            self.p1 = 0;
+            self.p2 = 0;
+            self.tie = 0;
 
-    // sets boxes as an array
-    self.boxes = [ {isX: false, isO: false},
-                   {isX: false, isO: false},
-                   {isX: false, isO: false},
-                   {isX: false, isO: false},
-                   {isX: false, isO: false},
-                   {isX: false, isO: false},
-                   {isX: false, isO: false},
-                   {isX: false, isO: false},
-                   {isX: false, isO: false}
-                 ];
-    
-    // attaches takeTurns to the controller
-    self.takeTurns = takeTurns;
+            //sets game status as game in progress
+            self.gameStatus = "Game in progress";
 
-    // attaches chooseBox to the controller
-    self.chooseBox = chooseBox;
+            // gets boxes array from firebase
+            self.boxes = (function(){
+                var ref = new Firebase('https://tictactoekate.firebaseio.com/boxes');
+                var boxes = $firebaseArray(ref);
+                console.log(boxes);
+                return boxes
+            })();
+            
+            // attaches takeTurns to the controller
+            self.takeTurns = takeTurns;
 
-    //attaches getWinner to the controller
-    self.getWinner = getWinner;
+            // attaches chooseBox to the controller
+            self.chooseBox = chooseBox;
 
-    //attaches resetBoard to the controller
-    self.resetBoard = resetBoard;
+            //attaches getWinner to the controller
+            self.getWinner = getWinner;
 
-    // alternates between players 1 and 2
-    function takeTurns() {
-        self.turn++;
-        if (self.turn % 2 === 0) {
-            return "o";
-        } else {
-            return "x";
-        } 
-    }
+            //attaches resetBoard to the controller
+            self.resetBoard = resetBoard;
 
-    // reacts to box being clicked if game is in progress
-    function chooseBox(index) {
-        if (self.gameStatus === "Game in progress") {
-            if ((self.boxes[index].isX === true) || (self.boxes[index].isO === true)) {
-            alert("Oops! This cell is already taken. Please select a different cell.");
-            return;
+            // alternates between players 1 and 2
+            function takeTurns() {
+                self.turn++;
+                if (self.turn % 2 === 0) {
+                    return "o";
+                } else {
+                    return "x";
+                } 
             }
-            var myTurn = takeTurns();
-            if (myTurn === "o") {
-                self.boxes[index].isO = true;
-            }
-            else if (myTurn === "x") {
-                self.boxes[index].isX = true;
-            }
-            self.getWinner();
-        }
-    }
 
-    //checks for a winner
-    function getWinner() {
-        //checks for x win
-        if (((self.boxes[0].isX === true) && (self.boxes[1].isX === true) && (self.boxes[2].isX === true)) ||
-            ((self.boxes[3].isX === true) && (self.boxes[4].isX === true) && (self.boxes[5].isX === true)) ||
-            ((self.boxes[6].isX === true) && (self.boxes[7].isX === true) && (self.boxes[8].isX === true)) ||
-            ((self.boxes[0].isX === true) && (self.boxes[3].isX === true) && (self.boxes[6].isX === true)) ||
-            ((self.boxes[1].isX === true) && (self.boxes[4].isX === true) && (self.boxes[7].isX === true)) ||
-            ((self.boxes[2].isX === true) && (self.boxes[5].isX === true) && (self.boxes[8].isX === true)) ||
-            ((self.boxes[0].isX === true) && (self.boxes[4].isX === true) && (self.boxes[8].isX === true)) ||
-            ((self.boxes[2].isX === true) && (self.boxes[4].isX === true) && (self.boxes[6].isX === true)))
-        {
-            self.gameStatus = "X wins!";
-            self.p1++;
-            return;
-        }
-        //checks for o win
-        if (((self.boxes[0].isO === true) && (self.boxes[1].isO === true) && (self.boxes[2].isO === true)) ||
-            ((self.boxes[3].isO === true) && (self.boxes[4].isO === true) && (self.boxes[5].isO === true)) ||
-            ((self.boxes[6].isO === true) && (self.boxes[7].isO === true) && (self.boxes[8].isO === true)) ||
-            ((self.boxes[0].isO === true) && (self.boxes[3].isO === true) && (self.boxes[6].isO === true)) ||
-            ((self.boxes[1].isO === true) && (self.boxes[4].isO === true) && (self.boxes[7].isO === true)) ||
-            ((self.boxes[2].isO === true) && (self.boxes[5].isO === true) && (self.boxes[8].isO === true)) ||
-            ((self.boxes[0].isO === true) && (self.boxes[4].isO === true) && (self.boxes[8].isO === true)) ||
-            ((self.boxes[2].isO === true) && (self.boxes[4].isO === true) && (self.boxes[6].isO === true)))
-        {
-            self.gameStatus = "O wins!";
-            self.p2++;
-            return;
-        }
-        //checks for tie
-        var cellEmpty = false;
-        for (var i = 0; i < self.boxes.length; i++) {
-            if ((self.boxes[i].isX === false) && (self.boxes[i].isO === false)) {
-            cellEmpty = true;
+            // reacts to box being clicked if game is in progress
+            function chooseBox(index) {
+                if (self.gameStatus === "Game in progress") {
+                    if ((self.boxes[index].isX === true) || (self.boxes[index].isO === true)) {
+                    alert("Oops! This cell is already taken. Please select a different cell.");
+                    return;
+                    }
+                    var myTurn = takeTurns();
+                    if (myTurn === "o") {
+                        self.boxes[index].isO = true;
+                    }
+                    else if (myTurn === "x") {
+                        self.boxes[index].isX = true;
+                    }
+                    self.getWinner();
+                }
             }
-        }
-        if (cellEmpty === false) {
-            self.gameStatus = "It's a tie!"
-        }
-    
-    }
 
-    //clears board when game is over
-    function resetBoard() {
-        for (var i = 0; i < self.boxes.length; i++) {
-            self.boxes[i].isX = false;
-            self.boxes[i].isO = false;
-        }
-        self.gameStatus = "Game in progress";
-    }
+            //checks for a winner
+            function getWinner() {
+                //checks for x win
+                if (((self.boxes[0].isX === true) && (self.boxes[1].isX === true) && (self.boxes[2].isX === true)) ||
+                    ((self.boxes[3].isX === true) && (self.boxes[4].isX === true) && (self.boxes[5].isX === true)) ||
+                    ((self.boxes[6].isX === true) && (self.boxes[7].isX === true) && (self.boxes[8].isX === true)) ||
+                    ((self.boxes[0].isX === true) && (self.boxes[3].isX === true) && (self.boxes[6].isX === true)) ||
+                    ((self.boxes[1].isX === true) && (self.boxes[4].isX === true) && (self.boxes[7].isX === true)) ||
+                    ((self.boxes[2].isX === true) && (self.boxes[5].isX === true) && (self.boxes[8].isX === true)) ||
+                    ((self.boxes[0].isX === true) && (self.boxes[4].isX === true) && (self.boxes[8].isX === true)) ||
+                    ((self.boxes[2].isX === true) && (self.boxes[4].isX === true) && (self.boxes[6].isX === true)))
+                {
+                    self.gameStatus = "X wins!";
+                    self.p1++;
+                    return;
+                }
+                //checks for o win
+                if (((self.boxes[0].isO === true) && (self.boxes[1].isO === true) && (self.boxes[2].isO === true)) ||
+                    ((self.boxes[3].isO === true) && (self.boxes[4].isO === true) && (self.boxes[5].isO === true)) ||
+                    ((self.boxes[6].isO === true) && (self.boxes[7].isO === true) && (self.boxes[8].isO === true)) ||
+                    ((self.boxes[0].isO === true) && (self.boxes[3].isO === true) && (self.boxes[6].isO === true)) ||
+                    ((self.boxes[1].isO === true) && (self.boxes[4].isO === true) && (self.boxes[7].isO === true)) ||
+                    ((self.boxes[2].isO === true) && (self.boxes[5].isO === true) && (self.boxes[8].isO === true)) ||
+                    ((self.boxes[0].isO === true) && (self.boxes[4].isO === true) && (self.boxes[8].isO === true)) ||
+                    ((self.boxes[2].isO === true) && (self.boxes[4].isO === true) && (self.boxes[6].isO === true)))
+                {
+                    self.gameStatus = "O wins!";
+                    self.p2++;
+                    return;
+                }
+                //checks for tie
+                var cellEmpty = false;
+                for (var i = 0; i < self.boxes.length; i++) {
+                    if ((self.boxes[i].isX === false) && (self.boxes[i].isO === false)) {
+                    cellEmpty = true;
+                    }
+                }
+                if (cellEmpty === false) {
+                    self.gameStatus = "It's a tie!"
+                    self.tie++;
+                }
+            
+            }
 
-}
+            //clears board when game is over
+            function resetBoard() {
+                for (var i = 0; i < self.boxes.length; i++) {
+                    self.boxes[i].isX = false;
+                    self.boxes[i].isO = false;
+                }
+                self.gameStatus = "Game in progress";
+            }
+
+        }
 
 
 
